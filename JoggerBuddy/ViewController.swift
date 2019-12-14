4c47a7d8db4b49608f3d2f69f3e3e07581417ad1
Jack Pope <jackrpope@gmail.com>
2019-12-14 13:10:44 -0800

    Initial Commit

---
+++ JoggerBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoggerBuddy
+//
+//  Created by Jack Pope on 12/14/19.
+//  Copyright © 2019 Jack Pope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
